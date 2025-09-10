6d7384e738c41287d3839f56d8a31739e494d03f
nikmalykhin <nik.malykhin@throughtworks.com>
2025-09-10 14:35:19 +0200

    add integration test

---
+++ backend/src/integrationTest/kotlin/jules/foundation/SampleIntegrationTest.kt
@@ -0,0 +1,12 @@
+package jules.foundation
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class SampleIntegrationTest :
+        StringSpec({
+            "sample integration test" {
+                // This is just a placeholder test to ensure the integration test setup works
+                true shouldBe true
+            }
+        })
